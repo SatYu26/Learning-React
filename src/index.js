bab64a8f72bc5a0e1c77d45f64354c55d7060fd1
SatYu26 <goyalsatyam8@gmail.com>
2021-02-21 02:46:41 +0530

    adding props

--- src/index.js
+++ src/index.js
@@ -2,16 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Hello() {
+function Hello(props) {
+  console.log(props);
   return (
     <div>
-      <h1>Welcome</h1>
-      <p>Lets Build</p>
+      <h1>Welcome to {props.library}</h1>
+      <p>Lets {props.message}</p>
+      <p>{props.number} props total</p>
     </div>
   );
 }
 
 ReactDOM.render(
-  <Hello />,
+  <Hello 
+  library="React" 
+  message="Have fun" 
+  number={3} 
+  />,
   document.getElementById('root')
 );
