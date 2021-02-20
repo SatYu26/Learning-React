ba7b198767c3903b072f48f5f8c30b1ff18769b3
SatYu26 <goyalsatyam8@gmail.com>
2021-02-21 03:06:31 +0530

    rendering lists

--- src/index.js
+++ src/index.js
@@ -2,23 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-function Lake({ name }) {
-  return (
-    <h1>{ name }</h1>
-  );
-}
+const lakeList = ["ganga", "jamuna", "saraswati"];
 
-function App() {
+function App({ lakes }) {
   return (
-  <div>
-    <Lake name="Lake taho" />
-    <Lake name="ganga" />
-    <Lake name="jamuna" />
-  </div>
+  <ul>
+    { lakes.map(lake => <li>{lake}</li>) }
+  </ul>
   );
 }
 
 ReactDOM.render(
-  <App />,
+  <App lakes={ lakeList } />,
   document.getElementById('root')
 );
