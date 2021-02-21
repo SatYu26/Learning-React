b189b6d4bc370b41a7a7354b14133548c9002bf0
SatYu26 <goyalsatyam8@gmail.com>
2021-02-21 18:03:38 +0530

    using useState

--- src/index.js
+++ src/index.js
@@ -1,26 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-const [first, second, third] = ["pop", "abc", "cde"];
-console.log(first);
-console.log(second);
-console.log(third);
-
-function Lake() {
-  return <h1>Lake</h1>;
-}
-
-function SkiResort() {
-  return <h1>SkiResort</h1>;
-}
-
 function App() {
+  const [status, setStatus] = useState("Open");
   return (
-    <React.Fragment>
-      <Lake />
-      <SkiResort />
-    </React.Fragment>
+    <div>
+      <h1>Status: {status}</h1>
+      <button onClick={() => setStatus("Closed")}>Closed</button>
+      <button onClick={() => setStatus("Open")}>Open</button>
+      <button onClick={() => setStatus("Back in 5")}>Break</button>
+    </div>
   );
 }
 
