3d6ce693f450fe5c1bcc3f02a2b2a80efaf97943
SatYu26 <goyalsatyam8@gmail.com>
2021-02-21 18:32:27 +0530

    working with useEffects

--- src/index.js
+++ src/index.js
@@ -1,29 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-function App() {
-  const [year, setYear] = useState("2050");
-  const [manager, setManager] = useState("Alex");
-  const [status, setStatus] = useState("Open");
+function Checkbox() {
+  const [checked, setChecked] = useState(false);
+  useEffect(() => {
+    alert(`checked: ${checked.toString()}`);
+  });
   return (
     <React.Fragment>
-      <div>
-        <h1>{year}</h1>
-        <button onClick={() => setYear(year + 1)}>change year</button>
-      </div>
-      <div>
-        <h1>Manager on duty: {manager}</h1>
-        <button onClick={() => setManager("Rock")}>Change Manager</button>
-      </div>
-      <div>
-        <h1>Status: {status}</h1>
-        <button onClick={() => setStatus("Closed")}>Closed</button>
-        <button onClick={() => setStatus("Open")}>Open</button>
-        <button onClick={() => setStatus("Back in 5")}>Break</button>
-      </div>
+      <input
+        type="Checkbox"
+        value={checked}
+        onChange={() => setChecked((checked) => !checked)}
+      />
+      {checked ? "checked" : "not checked"}
     </React.Fragment>
   );
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<Checkbox />, document.getElementById("root"));
