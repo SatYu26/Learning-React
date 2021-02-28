bc138c85851c1b0add08e5df4f9874ab9bec8882
SatYu26 <goyalsatyam8@gmail.com>
2021-02-28 23:44:57 +0530

    Configuring the router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { Home, About, Events, Contacts } from "./pages";
 
 function App() {
   return (
     <div>
-      <h1>Hello React</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/contacts" element={<Contacts />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+export function Home() {
+  return (
+    <div>
+      <h1>[Company website]</h1>
+    </div>
+  );
+}
+export function About() {
+  return (
+    <div>
+      <h1>[About Page]</h1>
+    </div>
+  );
+}
+
+export function Events() {
+  return (
+    <div>
+      <h1>[Events Page]</h1>
+    </div>
+  );
+}
+
+export function Contacts() {
+  return (
+    <div>
+      <h1>[Contacts Page]</h1>
+    </div>
+  );
+}
